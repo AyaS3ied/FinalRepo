6f965dec31a8dff780223bd409119db43d3ccede
- <as3ied@hotmail.com>
2016-10-18 16:35:45 +0200

    This is Adding new Aya Class again

--- src/j2se/xe/github/signup/MyNewAya.java
+++ src/j2se/xe/github/signup/MyNewAya.java
@@ -10,14 +10,17 @@ package j2se.xe.github.signup;
  * @author Administrator
  */
 
+
 public class MyNewAya {
     public static void main(String[] args) {
-        System.out.println("Yahia Editing 1");    
         
+        System.out.println("Aya 0");
+        System.out.println("Yahia Editing 1");    
+        System.out.println("Aya 1");
         System.out.println("ayaaa");
         
         
         System.out.println("Yahia Editing 2");    
-
+           System.out.println("Aya 2");
    }
 }
