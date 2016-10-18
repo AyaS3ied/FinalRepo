1dbfe4d865681243ecdae29f23c24c4945af6285
Administrator <Administrator@yahia-PC>
2016-10-18 16:16:44 +0200

    This is Adding Yahia Class File - Edit New Aya Class

--- src/j2se/xe/github/signup/MyNewAya.java
+++ src/j2se/xe/github/signup/MyNewAya.java
@@ -9,8 +9,15 @@ package j2se.xe.github.signup;
  *
  * @author Administrator
  */
+
 public class MyNewAya {
     public static void main(String[] args) {
+        System.out.println("Yahia Editing 1");    
+        
         System.out.println("ayaaa");
-    }
+        
+        
+        System.out.println("Yahia Editing 2");    
+
+   }
 }
